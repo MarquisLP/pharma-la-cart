a66af461ff3e7808f1010f16c771d300507e0d8f
nathan78906 <nathan.seebarran@gmail.com>
2020-04-25 19:19:51 -0400

    more prescriptions

--- routes/Prescriptions/prescription.js
+++ routes/Prescriptions/prescription.js
@@ -1,4 +1,5 @@
 module.exports = function(server) {
+    const { isLoggedin } = require('../route_utils')
     const Prescription = require('../../database/models/prescription')
     const PrescriptionItem = require('../../database/models/prescription_item')
     // const multer = require("multer");
@@ -37,8 +38,9 @@ module.exports = function(server) {
     //     }
     //   });
 
-    server.post('/api/prescriptions/', async function(req, res, next) {
+    server.post('/api/prescriptions/', isLoggedin, async function(req, res, next) {
       const reqPrescription = req.body
+      reqPrescription.user_name = req.session.user_name
 
       Prescription.create(reqPrescription, function(err, newPrescription){
         if (err){
@@ -58,6 +60,41 @@ module.exports = function(server) {
           }
         }
       });
-      
     })
+
+    server.get('/api/prescriptions/mine/', isLoggedin, async function(req, res, next) {
+      var me = req.session.user_name;
+
+      var query = Prescription.where({ user_name: me });
+      query.find(function (err, prescription_list) {
+        return res.status(200).json(prescription_list)
+      });
+    })
+
+    server.get('/api/prescriptions/:prescription_id', isLoggedin, async function(req, res, next) {
+      const reqPrescriptionId = req.params.prescription_id
+      var query  = Prescription.where({ _id: reqPrescriptionId });
+      query.findOne(function (err, prescription) {
+        if (prescription) {
+          return res.status(200).json(prescription)
+        }
+        return res.status(404).json("Not Found")
+      });
+    })
+
+    server.patch('/api/prescriptions/:prescription_id', isLoggedin, async function(req, res, next) {
+    const reqPrescriptionId = req.params.prescription_id
+    const reqStatus = req.body.status;
+    const reqPharmacyId = req.body.pharmacy_id;
+
+    var query  = Prescription.where({ _id: reqPrescriptionId });
+    var update = { status: reqStatus, pharmacy_id: reqPharmacyId };
+    var options = { new: true };
+    Prescription.findOneAndUpdate(query, update, options, function (err, prescription){
+      if (prescription) {
+        return res.status(200).json(prescription)
+      }
+      return res.status(404).json("Not Found")
+    });
+  })
 }
