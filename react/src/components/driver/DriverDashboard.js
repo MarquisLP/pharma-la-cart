de537b5660d5409883a9364632fc2748ee611cc3
Mark Padilla <markjjpadilla@gmail.com>
2020-04-25 19:08:29 -0400

    Implement temp Driver login for testing purposes

--- react/src/components/driver/DriverDashboard.js
+++ react/src/components/driver/DriverDashboard.js
@@ -1,20 +1,52 @@
 import React from 'react'
 import { Grid, CssBaseline } from '@material-ui/core'
+import axios from 'axios'
 import DeliveryList from './DeliveryList'
 
+axios.defaults.withCredentials = true
+
 class DriverDashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFinishedLoggingIn: false
+    }
+  }
+
+  // Temporary test account login
+  componentDidMount() {
+    axios.post(
+      'http://localhost:8080/api/sessions',
+      {
+        user_name: 'DriverTwo',
+        password: 'clear'
+      }
+    )
+      .then((response) => {
+        this.setState({
+          isFinishedLoggingIn: true
+        })
+      })
+  }
+
   render() {
     return (
       <>
         <CssBaseline />
-        <Grid
-          container
-          direction='column'
-          justify='center'
-          alignItems='center'
-        >
-          <DeliveryList />
-        </Grid>
+        {
+          this.state.isFinishedLoggingIn
+            ? (
+              <Grid
+                container
+                direction='column'
+                justify='center'
+                alignItems='center'
+              >
+                <DeliveryList />
+              </Grid>
+            )
+            : null
+        }
       </>
     )
   }
