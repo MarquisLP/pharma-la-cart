e960295101f82ae16c253f00219ed33bfc5ca231
Mark Padilla <markjjpadilla@gmail.com>
2020-04-25 23:22:23 -0400

    Fix merge conflicts in App.js

--- react/src/App.js
+++ react/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Register from './components/Register';
 import DriverDashboard from './components/driver/DriverDashboard'
 import DeliveryDetails from './components/driver/DeliveryDetails';
+import PharmacistDashboard from './components/pharmacist/PharmacistDashboard'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' component={Register} />
         <Route path='/driver/dashboard/' component={DriverDashboard} />
         <Route path='/driver/deliveries/:deliveryId' component={DeliveryDetails} />
+        <Route path='/pharmacist/dashboard' component={PharmacistDashboard} />
         </Switch>
       </BrowserRouter>
   );
