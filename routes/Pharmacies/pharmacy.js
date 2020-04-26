3c81aab60ef677fc4f2e528a5fabf71cbda2729d
nathan78906 <nathan.seebarran@gmail.com>
2020-04-25 21:37:27 -0400

    pharmacy's medicines

--- routes/Pharmacies/pharmacy.js
+++ routes/Pharmacies/pharmacy.js
@@ -2,6 +2,9 @@ module.exports = function(server) {
   const { isLoggedin } = require('../route_utils')
 
   const Pharmacy = require('../../database/models/pharmacy')
+  const Medicine = require('../../database/models/medicine')
+  const PharmacyMedicine = require('../../database/models/pharmacy_medicine')
+
   server.post('/api/pharmacies/', isLoggedin, async function(req, res, next) {
     const reqPharmacy = req.body
     Pharmacy.create(reqPharmacy, function(err, newPharmacy){
@@ -25,6 +28,43 @@ module.exports = function(server) {
       if (pharmacy) {
         return res.status(200).json(pharmacy)
       }
+    });
+  })
+
+  server.post('/api/pharmacies/:pharmacy_id/inventory/medicines/:medicine_id', isLoggedin, async function(req, res, next) {
+    const reqPharmacyId = req.params.pharmacy_id
+    const reqMedicineId = req.params.medicine_id
+    var obj = {
+      'medicine_id': reqMedicineId,
+      'pharmacy_id': reqPharmacyId
+    }
+
+    PharmacyMedicine.create(obj, function(err, newPharmacyMedicine){
+      if (err){
+        console.log(err);
+        return res.status(400).json("Bad Request");
+      }
+      if (newPharmacyMedicine){
+        res.status(201).json(newPharmacyMedicine)
+      }
+    });
+  })
+
+  server.get('/api/pharmacies/:pharmacy_id/inventory/medicines/', isLoggedin, async function(req, res, next) {
+    const reqPharmacyId = req.params.pharmacy_id
+    var query = PharmacyMedicine.where({ pharmacy_id: reqPharmacyId });
+    var items = [];
+
+    await query.find(async function (err, medicineid_list) {
+      console.log(medicineid_list);
+      for (var i=0; i<medicineid_list.length; i++) {
+        var currItem = medicineid_list[i];
+
+        var query = Medicine.where({ _id: currItem.medicine_id });
+        var medicine = await query.findOne();
+        items.push(medicine);
+      }  
+      return res.status(200).json(items); 
     });
   })
 }
